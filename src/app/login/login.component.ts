bcca677e7e1e8f1ab83c68e0dbafd6535b0ed32d
JeffChristian <jeffchristiansantos@gmail.com>
2022-07-22 18:41:20 -0300

    estilização do formulário de acesso, implementação da página de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser, faInfoCircle, faLock, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  faUser = faUser;
+  faInfoCircle = faInfoCircle;
+  faLock = faLock;
+  faEyeSlash = faEyeSlash;
 
   constructor() { }
 
